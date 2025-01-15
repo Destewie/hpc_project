9c499aa851d92d7a053f893f1341e04d1533ee2e
achf01 <annachiarafortuna01@gmail.com>
2025-01-15 17:16:31 +0100

    update volitive movement baricent weighted on weights + refactor rastigin

--- src/sequential_execution/src/sequential_FSS_simple.c
+++ src/sequential_execution/src/sequential_FSS_simple.c
@@ -6,16 +6,16 @@
 
 #define DIMENSIONS 2
 #define N_FISHES 30
-#define MAX_ITER 25
+#define MAX_ITER 100
 #define BOUNDS_MIN -20.0   // Minimum bound of the search space
-#define BOUNDS_MAX -1.0    // Maximum bound of the search space
+#define BOUNDS_MAX 10.0    // Maximum bound of the search space
 #define BOUNDS_MIN_W 0.1   // Minimum bound of the search space
 #define BOUNDS_MAX_W 10.0    // Maximum bound of the search space
 #define W_SCALE_MIN 1.0
 #define W_SCALE_MAX 10.0
-#define FUNCTION "min_sphere"   //TODO: Capire se, al posto di fare un controllo su una stringa, possiamo passare alle funzioni direttamente un puntatore ad una funzione (in modo comodo, se no lasciamo perdere)
-#define MULTIPLIER -1   // 1 in case of maximization, -1 in case of minimization
-#define A 10.0 //Rastrigin param
+#define FUNCTION "min_rastringin"   //TODO: Capire se, al posto di fare un controllo su una stringa, possiamo passare alle funzioni direttamente un puntatore ad una funzione (in modo comodo, se no lasciamo perdere)
+#define MULTIPLIER 1   // 1 in case of maximization, -1 in case of minimization
+#define A 10.0 //Rastringin param
 
 typedef struct {
     double position[DIMENSIONS];
@@ -87,7 +87,7 @@ double rosenbrok(double *x) {
     return sum;
 }
 
-double rastrigin(double *x) {
+double rastringin(double *x) {
     double sum = A * DIMENSIONS; // Start with A * DIM
     for (int i = 0; i < DIMENSIONS; i++) {
         sum += x[i] * x[i] - A * cos(2 * M_PI * x[i]);
@@ -118,7 +118,7 @@ double objective_function(double *x) {
         return min_sphere(x);
     } else if (strcmp(FUNCTION, "max_sphere") == 0) {
         return max_sphere(x);
-    } else if (strcmp(FUNCTION, "min_rastrigin") == 0) {
+    } else if (strcmp(FUNCTION, "min_rastringin") == 0) {
         return max_sphere(x);
     } else {
         return 0.0;
@@ -152,7 +152,7 @@ void initFish(Fish *fish) {
     fish->fitness = objective_function(fish->position)*MULTIPLIER;        // Fitness iniziale //TODO: capire qual è il valore migliore di inizializzazione
     fish->new_fitness = fish->fitness;
 
-    fish->max_individual_step = 0.7; //TODO: capire qual è il valore migliore di inizializzazione e come aggiornarlo dinamicamente
+    fish->max_individual_step = 1.5; //TODO: capire qual è il valore migliore di inizializzazione e come aggiornarlo dinamicamente
     fish->max_volitive_step = 0.2; //TODO: capire qual è il valore migliore di inizializzazione e come aggiornarlo dinamicamente
 }
 
@@ -279,7 +279,8 @@ void calculateBarycenter(Fish *fishArray, float *barycenter){
     for (int d = 0; d<DIMENSIONS; d++){
         for (int i = 0; i < N_FISHES; i++) {
             numerator[d] += fishArray[i].position[d] * fishArray[i].weight;
-            denominator[d] += fishArray[i].position[d];  
+            // denominator[d] += fishArray[i].position[d];  
+            denominator[d] += fishArray[i].weight;
         }
 
         if (denominator[d] != 0.0) {
@@ -309,7 +310,7 @@ void volitivePositionUpdateArray(Fish *fishArray, int shrink, float* barycenter)
                 fishArray[i].position[d] -= fishArray[i].max_volitive_step * rand_mult * (fishArray[i].position[d] - barycenter[d]);
             }
         }
-    } else {
+    } else {os: -0.001811      weight: 8.751085        fitness: -0.207873
         for (int i = 0; i < N_FISHES; i++) {
             for (int d = 0; d < DIMENSIONS; d++){ // TODO: change max individual step with another step in order to have the possibility to tune it
                 rand_mult= (double)rand() / (double)RAND_MAX; //numero qualsiasi tra 0 e 1 
