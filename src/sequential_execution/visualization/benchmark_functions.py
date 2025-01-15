236ccd113d56e83500dfde0b19800afc49e4b46e
achf01 <annachiarafortuna01@gmail.com>
2025-01-15 18:07:26 +0100

    aggiunta visualizzazione rosenbrok

--- src/sequential_execution/visualization/benchmark_functions.py
+++ src/sequential_execution/visualization/benchmark_functions.py
@@ -9,7 +9,7 @@ def max_spherical_function(x, y=0):
     return -(x**2 + y**2)
 
 # TODO: Generalize in n dimensions
-def min_rastrigin_function(*args):
+def min_rastringin_function(*args):
     if len(args) == 1:
         x = args[0]
         return 10 + x**2 - 10 * np.cos(2 * np.pi * x)
@@ -19,7 +19,10 @@ def min_rastrigin_function(*args):
     else:
         raise TypeError("Expected 1 or 2 arguments.")
 
+def min_rosenbrock_function(x, y):
+    return 100 * (y - x**2)**2 + (1 - x)**2
 
-# def min_rastrigin_function(x, A=10):
+
+# def min_rastringin_function(x, A=10):
 #     n = len(x)  # Number of dimensions
 #     return A * n + sum(xi**2 - A * np.cos(2 * np.pi * xi) for xi in x)
