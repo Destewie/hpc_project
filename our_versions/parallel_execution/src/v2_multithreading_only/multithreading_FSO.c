2395f997cc830a9731fb580aa29379bbe160c5ad
FedeDes <federico.ds25@gmail.com>
2025-05-08 11:35:05 +0200

    cambio funzione

--- our_versions/parallel_execution/src/v2_multithreading_only/multithreading_FSO.c
+++ our_versions/parallel_execution/src/v2_multithreading_only/multithreading_FSO.c
@@ -15,7 +15,7 @@
 #include <mpi.h> //l'unico utilizzo qui è per il wtime
 
 
-#define FUNCTION "min_sphere"   //TODO: Capire se, al posto di fare un controllo su una stringa, possiamo passare alle funzioni direttamente un puntatore ad una funzione (in modo comodo, se no lasciamo perdere)
+#define FUNCTION "min_rosenbrok"   //TODO: Capire se, al posto di fare un controllo su una stringa, possiamo passare alle funzioni direttamente un puntatore ad una funzione (in modo comodo, se no lasciamo perdere)
 #define MULTIPLIER -1   // 1 in case of maximization, -1 in case of minimization
 
 #define BOUNDS_MIN -70.0   // Minimum bound of the search space
