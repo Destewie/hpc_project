263eea2ffa6f5d2011507b8fabe4e5b285ba69d2
FedeDes <federico.ds25@gmail.com>
2025-05-26 12:52:21 +0200

    16 processi 32 thread

--- our_versions/parallel_execution/src/v3_mpi_openmp/mpi_openmp_FSO.sh
+++ our_versions/parallel_execution/src/v3_mpi_openmp/mpi_openmp_FSO.sh
@@ -15,10 +15,10 @@
 #PBS -l walltime=06:00:00
 
 # chunks (~nodes) : cores per chunk : shared memory per chunk (?)
-#PBS -l select=32:ncpus=128:mem=2gb
+#PBS -l select=16:ncpus=32:mem=2gb
 #PBS -l place=scatter
 
-export OMP_NUM_THREADS=128
+export OMP_NUM_THREADS=32
 export OMP_PROC_BIND=close
 export OMP_PLACES=cores
 export OMP_SCHEDULE="dynamic, 1"
@@ -35,6 +35,6 @@ mpicc $C_PROGRAM_PATH -g -Wall -fopenmp -lm -std=c99 -o $EXECUTABLE_PATH_AND_NAM
 
 # run
 # <"Usage: N_FISHES_PER_PROCESS DIMENSIONS MAX_ITER UPDATE_FREQUENCY">
-mpirun.actual -n 32 $EXECUTABLE_PATH_AND_NAME 2000 1000 200 1 scatter
+mpirun.actual -n 16 $EXECUTABLE_PATH_AND_NAME 4000 1000 200 1 scatter
 
 
