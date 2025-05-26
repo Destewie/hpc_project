38d4ba8e6f9cc43cc81a86e1cc0c6e1a833f45a8
FedeDes <federico.ds25@gmail.com>
2025-05-26 12:14:00 +0200

    16 processi 8 thread

--- our_versions/parallel_execution/src/v3_mpi_openmp/mpi_openmp_FSO.sh
+++ our_versions/parallel_execution/src/v3_mpi_openmp/mpi_openmp_FSO.sh
@@ -15,10 +15,10 @@
 #PBS -l walltime=06:00:00
 
 # chunks (~nodes) : cores per chunk : shared memory per chunk (?)
-#PBS -l select=16:ncpus=4:mem=2gb
+#PBS -l select=16:ncpus=8:mem=2gb
 #PBS -l place=scatter
 
-export OMP_NUM_THREADS=4
+export OMP_NUM_THREADS=8
 export OMP_PROC_BIND=close
 export OMP_PLACES=cores
 export OMP_SCHEDULE="dynamic, 1"
