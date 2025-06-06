7f3d3f864083083233ef154bbe66e82f32ec33b4
Fortuna <annachiara.fortuna@hpc-head-n2.unitn.it>
2025-06-06 13:53:04 +0200

    aa

--- implementations/hybrid/mpi_openmp_FSO.sh
+++ implementations/hybrid/mpi_openmp_FSO.sh
@@ -15,10 +15,10 @@
 #PBS -l walltime=06:00:00
 
 # chunks (~nodes) : cores per chunk : shared memory per chunk (?)
-#PBS -l select=2:ncpus=4:mem=4gb
-#PBS -l place=scatter
+#PBS -l select=1:ncpus=4:mem=4gb
+#PBS -l place=pack
 
-export OMP_NUM_THREADS=4
+export OMP_NUM_THREADS=20
 export OMP_PROC_BIND=close
 export OMP_PLACES=cores
 export OMP_SCHEDULE="static, 1"
@@ -35,6 +35,6 @@ mpicc $C_PROGRAM_PATH -g -Wall -fopenmp -lm -std=c99 -o $EXECUTABLE_PATH_AND_NAM
 
 # run
 # <"Usage: N_FISHES_PER_PROCESS DIMENSIONS MAX_ITER UPDATE_FREQUENCY">
-mpirun.actual -n 2 $EXECUTABLE_PATH_AND_NAME 4000 1000 200 1 scatter
+mpirun.actual -n 4 $EXECUTABLE_PATH_AND_NAME 4000 1000 200 1 pack
 
 
