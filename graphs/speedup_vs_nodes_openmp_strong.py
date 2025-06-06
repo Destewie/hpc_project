c413ad5f2b2aa3534ebd75e1bbedfee95b6b0c08
achf01 <annachiarafortuna01@gmail.com>
2025-06-06 19:09:39 +0200

    graphs

---
+++ graphs/speedup_vs_nodes_openmp_strong.py
@@ -0,0 +1,45 @@
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import defaultdict
+
+# Load your JSON data
+with open('../implementations/hybrid/results_modified.json') as f:
+    data = json.load(f)
+
+
+# Group data by total_fishes
+grouped_data = defaultdict(list)
+
+for run_id, values in data.items():
+    if values["cores"] == 1:
+        total_fishes = values["total_fishes"]
+        nodes = values["nodes"]
+        speedup = values["speedup"]
+        grouped_data[total_fishes].append((nodes, speedup))
+
+# Plotting
+plt.figure(figsize=(8, 8))
+
+for total_fishes, points in grouped_data.items():
+    # Sort by number of cores for consistent lines
+    points.sort(key=lambda x: x[0])
+    cores, efficiencies = zip(*points)
+    plt.plot(cores, efficiencies, marker='o', label=f'{total_fishes} fishes')
+
+
+max_val = max(64,75)
+plt.plot([0, max_val], [0, max_val], 'k--', label="linear speedup")
+
+plt.ylim(0, 75)
+plt.xticks([1, 2, 4, 8, 16, 32, 64])
+plt.grid(True, which='both', axis='x')
+plt.xlabel('Number of Nodes')
+plt.ylabel('Speedup')
+plt.title(f"Speedup vs. Nodes")
+plt.legend(title='Total Fishes')
+plt.grid(True)
+plt.tight_layout()
+# plt.show()
+plt.savefig(f"images/speedup_vs_nodes_openmp_strong.png", dpi=100)
+
