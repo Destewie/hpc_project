fdbaf5f08c22a4f3a0ebf3975bb9df847113d257
FedeDes <federico.ds25@gmail.com>
2025-06-07 12:21:55 +0200

    grafici e script per i tempi e le efficiencies comparate tra tutte le implementazioni

--- implementations/hybrid/mpi_openmp_FSO.sh
+++ implementations/hybrid/mpi_openmp_FSO.sh
@@ -15,10 +15,10 @@
 #PBS -l walltime=06:00:00
 
 # chunks (~nodes) : cores per chunk : shared memory per chunk (?)
-#PBS -l select=1:ncpus=4:mem=4gb
+#PBS -l select=32:ncpus=4:mem=2gb
 #PBS -l place=pack
 
-export OMP_NUM_THREADS=20
+export OMP_NUM_THREADS=4
 export OMP_PROC_BIND=close
 export OMP_PLACES=cores
 export OMP_SCHEDULE="static, 1"
@@ -31,10 +31,8 @@ readonly EXECUTABLE_PATH_AND_NAME=~/hpc_project/implementations/hybrid/mpi_openm
 module load mpich-3.2
 
 # build
-mpicc $C_PROGRAM_PATH -g -Wall -fopenmp -lm -std=c99 -o $EXECUTABLE_PATH_AND_NAME 
+mpicc $C_PROGRAM_PATH -g -Wall -fopenmp -lm -std=c99 -o $EXECUTABLE_PATH_AND_NAME
 
 # run
 # <"Usage: N_FISHES_PER_PROCESS DIMENSIONS MAX_ITER UPDATE_FREQUENCY">
-mpirun.actual -n 4 $EXECUTABLE_PATH_AND_NAME 4000 1000 200 1 pack
-
-
+mpirun.actual -n 32 $EXECUTABLE_PATH_AND_NAME 2000 1000 200 1 pack
