f07304d9912c86c5d092d6f6610da033b638d50d
FedeDes <federico.ds25@gmail.com>
2025-06-07 12:34:25 +0200

    different params

--- implementations/hybrid/mpi_openmp_FSO.sh
+++ implementations/hybrid/mpi_openmp_FSO.sh
@@ -15,10 +15,10 @@
 #PBS -l walltime=06:00:00
 
 # chunks (~nodes) : cores per chunk : shared memory per chunk (?)
-#PBS -l select=32:ncpus=4:mem=2gb
+#PBS -l select=16:ncpus=8:mem=2gb
 #PBS -l place=pack
 
-export OMP_NUM_THREADS=4
+export OMP_NUM_THREADS=8
 # export OMP_PROC_BIND=close
 # export OMP_PLACES=cores
 export OMP_SCHEDULE="static, 1"
@@ -35,4 +35,4 @@ mpicc $C_PROGRAM_PATH -g -Wall -fopenmp -lm -std=c99 -o $EXECUTABLE_PATH_AND_NAM
 
 # run
 # <"Usage: N_FISHES_PER_PROCESS DIMENSIONS MAX_ITER UPDATE_FREQUENCY">
-mpirun.actual -n 32 $EXECUTABLE_PATH_AND_NAME 2000 1000 200 1 pack
+mpirun.actual -n 16 $EXECUTABLE_PATH_AND_NAME 4000 1000 200 1 pack
